3af54e73bc24c333923f31006334b500d8e7eba1
Glen McCallum <glen.mccallum@protonmail.com>
2019-02-12 09:01:18 -0800

    Added Sock-Merchant
    - first hackerrank interview prep problem

---
+++ Interview-Preparation-Kit/Warm-up-Challenges/Sock-Merchant/SockMerchant.UnitTests/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+
+namespace SockMerchant.UnitTests
+{
+    public class SolutionTests
+    {
+        public class TheSockMerchantMethod
+        {
+            [Theory]
+            [InlineData(9, new int[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 }, 3)]
+            [InlineData(7, new int[] { 1, 2, 1, 2, 1, 3, 2 }, 2)]
+            [InlineData(10, new int[] { 1, 1, 3, 1, 2, 1, 3, 3, 3, 3 }, 4)]
+            public void ComputesCorrectResponse(int numSocks, int[] socks, int numPairsResult)
+            {
+                Assert.Equal(Solution.sockMerchant(numSocks, socks), numPairsResult);
+            }
+
+        }
+
+    }
+}
---
+++ Interview-Preparation-Kit/Warm-up-Challenges/Sock-Merchant/SockMerchant/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SockMerchant
+{
+    public class Solution
+    {
+        public static int sockMerchant(int n, int[] ar)
+        {
+            var sockCount = new Dictionary<int, int>();
+
+            foreach (int sock in ar)
+            {
+                if (sockCount.ContainsKey(sock))
+                {
+                    sockCount[sock]++;
+                    continue;
+                }
+
+                sockCount.Add(sock, 1);
+            }
+
+            int pairCount = 0;
+
+            foreach (var sockColour in sockCount)
+            {
+                pairCount += sockColour.Value / 2;
+            }
+
+            return pairCount;
+        }
+    }
+}
