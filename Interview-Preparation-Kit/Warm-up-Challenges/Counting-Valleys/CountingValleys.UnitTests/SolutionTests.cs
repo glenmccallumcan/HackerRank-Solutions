cb9644430289a3e89008deb6a52a99704cbf7396
Glen McCallum <glen.mccallum@protonmail.com>
2019-02-13 08:10:28 -0800

    Added Counting-Valleys
    - another interiew prep warm-up problem

---
+++ Interview-Preparation-Kit/Warm-up-Challenges/Counting-Valleys/CountingValleys.UnitTests/SolutionTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace CountingValleys.UnitTests
+{
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData(8, "UDDDUDUU", 1)]
+        public void ComputesCorrectResponse(int n, string s, int numValleys)
+        {
+
+            Assert.Equal(Solution.countingValleys(n, s), numValleys);
+        }
+    }
+}
---
+++ Interview-Preparation-Kit/Warm-up-Challenges/Counting-Valleys/CountingValleys/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CountingValleys
+{
+    public class Solution
+    {
+        public static int countingValleys(int n, string s)
+        {
+            int valleyCounter = 0;
+            int runningElevation = 0;
+            bool alreadyinValley = false;
+
+            char[] steps = s.ToCharArray();
+
+            foreach (char step in steps)
+            {
+                if (step == 'U')
+                {
+                    runningElevation++;
+                }
+
+                if (step == 'D')
+                {
+                    runningElevation--;
+                }
+
+                if (runningElevation < 0 && !alreadyinValley)
+                {
+                    valleyCounter++;
+                    alreadyinValley = true;
+                }
+
+                if(runningElevation == 0)
+                {
+                    alreadyinValley = false;
+                }
+            }
+            
+            return valleyCounter;
+        }
+    }
+}
