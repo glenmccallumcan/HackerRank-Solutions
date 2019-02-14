4b0802e31313000de32729541b652fd42560c701
Glen McCallum <glen.mccallum@protonmail.com>
2019-02-14 08:03:59 -0800

    Added Jumping on the Clouds
    - another interview prep warm-up question

---
+++ Interview-Preparation-Kit/Warm-up-Challenges/Jumping-on-the-Clouds/JumpingOnTheClouds.UnitTests/SolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace JumpingOnTheClouds.UnitTests
+{
+    public class SolutionTests
+    {
+        public class TheJumpingOnCloudsMethod
+        {
+            [Theory]
+            [InlineData(new int[] { 0, 0, 1, 0, 0, 1, 0 }, 4)]
+            [InlineData(new int[] { 0, 0, 0, 0, 1, 0 }, 3)]
+            [InlineData(new int[] { 0, 1, 0, 0, 0, 1, 0 }, 3)]
+            [InlineData(new int[] { 0, 0, 0, 1, 0, 0 }, 3)]
+            public void ComputesCorrectResponse(int[] c, int numberConsetive)
+            {
+                Assert.Equal(Solution.jumpingOnClouds(c), numberConsetive);
+            }
+
+        }
+    }
+}
---
+++ Interview-Preparation-Kit/Warm-up-Challenges/Jumping-on-the-Clouds/JumpingOnTheClouds/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace JumpingOnTheClouds
+{
+    public class Solution
+    {
+        public static int jumpingOnClouds(int[] c)
+        {
+            int jumpCounter = 0;
+
+            int i = 0;
+            while(i < c.Length)
+            {
+                if(i == c.Length - 1 && c[i] == 0)
+                {
+                    break;
+                }
+                else if (i + 2 < c.Length && c[i + 2] == 0)
+                {
+                    jumpCounter++;
+                    i = i + 2;
+                }else if (i + 1 < c.Length && c[i + 1] == 0)
+                {
+                    jumpCounter++;
+                    i = i + 1;
+                }
+            }
+            return jumpCounter;
+        }
+    }
+}
