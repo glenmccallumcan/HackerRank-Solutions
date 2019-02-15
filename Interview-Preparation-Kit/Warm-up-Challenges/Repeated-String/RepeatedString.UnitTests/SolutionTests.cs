fa542f7c83a61e4e109c7c98a19e461c64cf6e74
Glen McCallum <glen.mccallum@protonmail.com>
2019-02-15 09:04:01 -0800

    Added Repeated String
    - another inteview prep warm-up problem

---
+++ Interview-Preparation-Kit/Warm-up-Challenges/Repeated-String/RepeatedString.UnitTests/SolutionTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace RepeatedString.UnitTests
+{
+    public class SolutionTests
+    {
+        public class TheRepeatedStringMethod
+        {
+            [Theory]
+            [InlineData("a", 1000000000000, 1000000000000)]
+            [InlineData("aba", 10, 7)]
+            public void ComputesCorrectResponse(string s, long n, long result)
+            {
+                Assert.Equal(Solution.repeatedString(s, n), result);
+            }
+        }
+    }
+}
---
+++ Interview-Preparation-Kit/Warm-up-Challenges/Repeated-String/RepeatedString/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace RepeatedString
+{
+    public class Solution
+    {
+
+        public static long repeatedString(string s, long n)
+        {
+
+            Func<string, int> getNumberOfAs = (str) => str.ToList<char>().Where<char>(x => x == 'a').Count<char>();
+
+            // count the as in string
+            long asInString = getNumberOfAs(s);
+
+            // divide n by the length of the string
+            long multiplesOfAs = n / s.Length;
+
+            // times result by number of as in string -> first set of as
+            long numberOfAs = multiplesOfAs * asInString;
+
+            // mod n by the length of the string -> mod result
+            long modResult =  n % s.Length;
+
+            // count the number of as in the first modresult characters in the string
+            return numberOfAs += getNumberOfAs(s.Substring(0, unchecked((int)modResult)));
+        }
+
+        
+    }
+}
