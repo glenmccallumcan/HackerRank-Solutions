2bab4b153ed98127bb043ccf8f6283a5ae3fb7f6
Glen McCallum <glen.mccallum@protonmail.com>
2019-02-18 10:18:23 -0800

    Added 2D Array DS
    - on to interview prep array problems
    - this one ended up a little tricky. Not
    for the problem. Just the testing with xunit
    of a jagged array input was more involved

---
+++ Interview-Preparation-Kit/Arrays/2D-Array-DS/2DArrayDS.UnitTests/SolutionTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Extensions;
+
+namespace _2DArrayDS.UnitTests
+{
+    public class SolutionTests
+    {
+        public class TheHourglassSumMethod
+        {
+            public static IEnumerable<object[]> HourglassData
+            {
+                get
+                {
+                    return new[]
+                    {
+                        new object[]
+                        {
+                            new int[6][] {new int[6] {-1, -1, 0, -9, -2, -2 },
+                                        new int[6] { -2, -1, -6, -8, -2, -5 },
+                                        new int[6] { -1, -1, -1, -2, -3, -4 },
+                                        new int[6] { -1, -9, -2, -4, -4, -5 },
+                                        new int[6] { -7, -3, -3, -2, -9, -9 },
+                                        new int[6] { -1, -3, -1, -2, -4, -5 } } as Object,
+                            -6
+                        }
+                    };
+                }
+            }
+            
+            [Theory]
+            [MemberData(nameof(HourglassData))]
+            public void ComputesCorrectResponse(int [][] arr, int sum)
+            {
+                Assert.Equal(Solution.hourglassSum(arr), sum);
+            }
+        }
+    }
+}
---
+++ Interview-Preparation-Kit/Arrays/2D-Array-DS/2DArrayDS/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _2DArrayDS
+{
+    public class Solution
+    {
+        public static int hourglassSum(int[][] arr)
+        {
+            int maxHourglassSum = int.MinValue;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    maxHourglassSum =
+                        getHourglassSum(arr, i, j) > maxHourglassSum ?
+                            getHourglassSum(arr, i, j) :
+                            maxHourglassSum;
+                }
+            }
+            return maxHourglassSum;
+        }
+
+        static int getHourglassSum(int[][] arr, int i, int j)
+        {
+            return arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                            + arr[i + 1][j + 1]
+                + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+        }
+    }
+}
