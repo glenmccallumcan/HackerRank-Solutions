5a492b1b03cc3a30f95dd8827595859cf5314ded
Glen McCallum <glen.mccallum@protonmail.com>
2019-02-19 07:53:13 -0800

    Added Left-Rotation Solution
    - from the interview prep array problems
    - 22m 06s
    - wrote solution twice. first one timed out on
    large test case.
    - had to rotate all the ints in one operation,
    not one by one in a helper method

---
+++ Interview-Preparation-Kit/Arrays/Left-Rotation/LeftRotation.UnitTests/SolutionTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+
+namespace LeftRotation.UnitTests
+{
+    public class SolutionTests
+    {
+        public class TheRotLeftMethod
+        {
+            [Theory]
+            [InlineData(new int[] { 1, 2, 3, 4, 5 }, 4, new int[] { 5, 1, 2, 3, 4 } )]
+            public static void ComputesCorrectResponse(int[] a, int d, int[] result)
+            {
+                Assert.Equal<int[]>(Solution.rotLeft(a, d), result);
+            }
+        }
+    }
+}
---
+++ Interview-Preparation-Kit/Arrays/Left-Rotation/LeftRotation/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeftRotation
+{
+    public class Solution
+    {
+        public static int[] rotLeft(int[] a, int d)
+        {
+            var temps = new int[d];
+
+            // put all the ints removed from the front in an array
+            for (int i = 0; i < d; i++)
+            {
+                temps[i] = a[i];
+            }
+
+            // shift remaining ints down
+            for (int i = d; i < a.Length; i++)
+            {
+                a[i - d] = a[i];
+            }
+
+            // add rotated ints back on to the end
+            int index = 0;
+            for (int i = a.Length - d; i < a.Length; i++)
+            {
+                a[i] = temps[index++];
+            }
+
+            return a;
+        }
+    }
+}
