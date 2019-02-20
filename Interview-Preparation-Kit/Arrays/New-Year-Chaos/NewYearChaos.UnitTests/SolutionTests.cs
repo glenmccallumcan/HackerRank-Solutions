6093415815d329c5790739760f270495148d20cc
Glen McCallum <glen.mccallum@protonmail.com>
2019-02-20 08:43:22 -0800

    Added solution for New Year Chaos
    - I bombed this one
    - gave up after an hour and looked
    and the editorial.
    - even after that I was slow in getting it
    - my problem is getting all of the relevant
    information from the wording

---
+++ Interview-Preparation-Kit/Arrays/New-Year-Chaos/NewYearChaos.UnitTests/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+
+namespace NewYearChaos.UnitTests
+{
+    public class SolutionTests
+    {
+        public class TheMinimumBribesMethod
+        {
+            [Theory]
+            [InlineData(new int[] { 2, 1, 5, 3, 4 }, 3)]
+            [InlineData(new int[] { 2, 5, 1, 3, 4 }, -1)]
+            [InlineData(new int[] { 1, 2, 3, 4, 5 }, 0)]
+            [InlineData(new int[] { }, 0)]
+            [InlineData(new int[] { 5, 1, 2, 3, 7, 8, 6, 4 }, -1)]
+            [InlineData(new int[] { 1, 2, 5, 3, 7, 8, 6, 4 }, 7)]
+            public static void ComputesCorrectResponse(int[] q, int minBribes)
+            {
+                Assert.Equal<int>(Solution.GetMinBribes(q), minBribes);
+            }
+        }
+    }
+}
---
+++ Interview-Preparation-Kit/Arrays/New-Year-Chaos/NewYearChaos/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("NewYearChaos.UnitTests")]
+
+namespace NewYearChaos
+{
+    
+
+    public class Solution
+    {
+        static void minimumBribes(int[] q)
+        {
+            var bribes = GetMinBribes(q);
+
+            if (bribes == -1)
+            {
+                Console.WriteLine("Too chaotic");
+            } else
+            {
+                Console.WriteLine(bribes);
+            }
+        }
+
+        internal static int GetMinBribes(int[] q)
+        {
+            int n = q.Length;
+            int cnt = 0;
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (q[i] != i + 1)
+                {
+                    if (((i - 1) >= 0) && q[i - 1] == (i + 1))
+                    {
+                        cnt++;
+                        q[i - 1] = q[i];
+                        q[i] = i + 1;
+                    }
+                    else if (((i - 2) >= 0) && q[i - 2] == (i + 1))
+                    {
+                        cnt += 2;
+                        q[i - 2] = q[i - 1];
+                        q[i - 1] = q[i];
+                        q[i] = i + 1;
+                    }
+                    else
+                    {
+                        return -1;
+                    }
+                }
+            }
+            return cnt;
+        }
+    }
+}
