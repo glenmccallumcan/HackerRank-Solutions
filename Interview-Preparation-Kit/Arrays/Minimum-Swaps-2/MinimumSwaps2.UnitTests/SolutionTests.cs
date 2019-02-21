a81f0c0ead22d6d84e2399eae97e61cc73465339
Glen McCallum <glen.mccallum@protonmail.com>
2019-02-21 09:52:31 -0800

    Added Minimum Swaps 2
    - took me over an hour
    - I overcomplicated the alorithm. Put to much emphasis on minumum number
    of swaps. was thinking of swapping the max out of place with the min out
    of place (provided the swap results in correct placement of one)
    - so I looked at some of the solutions in the discussion and on the web.
    they were just moving from left to right through they array. so I did
    that with the C# dict lookup to get the correct index
    - also read the editorial. something in there about finding cycles. I'd
    like to figure that out.
    - editorial also mentioned sorting the array and tracking the steps.
    that is good but it would have to be the most optimal sorting algorithm in
    order to get the minimum steps

---
+++ Interview-Preparation-Kit/Arrays/Minimum-Swaps-2/MinimumSwaps2.UnitTests/SolutionTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MinimumSwaps2.UnitTests
+{
+    public class SolutionTests
+    {
+        public class TheMinimumSwapsMethod
+        {
+            
+
+            [Theory]
+            [InlineData(new int[] { 4, 3, 1, 2 }, 3)]
+            [InlineData(new int[] { 2, 3, 4, 1, 5 }, 3)]
+            [InlineData(new int[] { 1, 3, 5, 2, 4, 6, 7 }, 3)]
+            public static void ComputesCorrectResponse(int[] arr, int answer)
+            {
+                Assert.Equal<int>(Solution.minimumSwaps(arr), answer);
+            }
+        }
+
+        public class TheConvertArrToDictMethod
+        {
+            public static IEnumerable<object[]> DictData
+            {
+                get
+                {
+                    return new[]
+                    {
+                        new object[]
+                        {
+                            new int[] { 4, 3, 1, 2 } as Object,
+                            new Dictionary<int, int> { { 4, 0 }, { 3, 1 }, { 1, 2 }, { 2, 3 } }
+                        }
+                    };
+                }
+            }
+
+            [Theory]
+            [MemberData(nameof(DictData))]
+            public static void ConvertsArrayToDict(int[] arr, Dictionary<int,int> output)
+            {
+                Assert.Equal(Solution.convertArrToDict(arr), output);
+            }
+        }
+    }
+}
---
+++ Interview-Preparation-Kit/Arrays/Minimum-Swaps-2/MinimumSwaps2/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("MinimumSwaps2.UnitTests")]
+
+namespace MinimumSwaps2
+{
+    public class Solution
+    {
+        internal static int minimumSwaps(int[] arr)
+        {
+            var arrayLookup = convertArrToDict(arr);
+            var counter = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] != i + 1)
+                {
+                    var correctIndex = arrayLookup[i + 1];
+
+                    //swap in the dict
+                    arrayLookup[i + 1] = i;
+                    arrayLookup[arr[i]] = correctIndex;
+
+                    //swap in the array
+                    int temp = arr[i];
+                    arr[i] = arr[correctIndex];
+                    arr[correctIndex] = temp;
+
+                    //increment counter
+                    counter++;
+                }
+            }
+            
+            return counter;
+        }
+
+        internal static Dictionary<int, int> convertArrToDict(int[] arr)
+        {
+            var result = new Dictionary<int, int>();
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                result.Add(arr[i], i);
+            }
+            return result;
+        }
+
+    }
+}
