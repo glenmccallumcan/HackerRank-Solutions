10ecf6e15d567505050611824d5602266a033e43
Glen McCallum <glen.mccallum@protonmail.com>
2019-04-11 11:27:56 -0700

    Added Array-Manipulation Solutioin

---
+++ Interview-Preparation-Kit/Arrays/Array-Manipulation/ArrayManipulation.UnitTests/SolutionTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using ArrayManipulation;
+using Xunit;
+
+namespace ArrayManipulation.UnitTests
+{
+    public class SolutionTests
+    {
+        public class TheArrayManipulationMethod
+        {
+            public static int ArrayLength = 5;
+            public static IEnumerable<object[]> QueryData
+            {
+                get
+                {
+                    return new[]
+                    {
+                        new object[]
+                        {
+                            5,
+                            new int[3][] {new int[3] { 1, 2, 100 },
+                                        new int[3] { 2, 5, 100 },
+                                        new int[3] { 3, 4, 100 } } as Object,
+                            200
+                        }
+                    };
+                }
+            }
+
+            [Theory]
+            [MemberData(nameof(QueryData))]
+            public void ComputesCorrectResponse(int n, int[][] queries, long result)
+            {
+                Assert.Equal(Solution.arrayManipulation(n, queries), result);
+            }
+        }
+    }
+}
---
+++ Interview-Preparation-Kit/Arrays/Array-Manipulation/ArrayManipulation/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArrayManipulation
+{
+    public class Solution
+    {
+        public static long arrayManipulation(int n, int[][] queries)
+        {
+            var prefixSumArray = new long[n];
+
+            foreach (var operation in queries )
+            {
+                int a = operation[0] - 1;
+                int b = operation[1];
+                int k = operation[2];
+
+                prefixSumArray[a] += k;
+                if (b < n)
+                {
+                    prefixSumArray[b] -= k;
+                }
+            }
+
+            long sum = 0;
+            long max = 0;
+            for(int i = 0;i<prefixSumArray.Length;i++)
+            {
+                sum += prefixSumArray[i];
+                max = sum > max ? sum : max;
+            }
+
+            return max;
+        }
+    }
+
+    
+}
