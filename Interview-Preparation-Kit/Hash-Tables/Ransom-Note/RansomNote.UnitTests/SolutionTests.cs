336223ff32cc5e8a92b65c8de97cd6268d5c3b30
Glen McCallum <glen.mccallum@protonmail.com>
2019-04-12 08:56:45 -0700

    Added solution for Ransom Note problem.
    - brute force using hashtable
    - could probably have found a more elegant
    solution using linq

---
+++ Interview-Preparation-Kit/Hash-Tables/Ransom-Note/RansomNote.UnitTests/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+
+namespace RansomNote.UnitTests
+{
+    public class SolutionTests
+    {
+        public class TheCheckMagazineMethod
+        {
+            [Theory]
+            [InlineData(new string[] { "ive", "got", "a", "lovely", "bunch", "of", "coconuts" },
+                new string[] { "ive", "got", "some", "coconuts" },
+                "No")]
+            [InlineData(new string[] { "two", "times", "three", "is", "not", "four" },
+                new string[] { "two", "times", "two", "is", "four" },
+                "No")]
+            public void ComputesCorrectResponse(string[] magazine, string[] note, string answer)
+            {
+                Assert.Equal(Solution.getCheckMagazineResult(magazine, note), answer);
+            }
+        }
+    }
+}
---
+++ Interview-Preparation-Kit/Hash-Tables/Ransom-Note/RansomNote/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RansomNote
+{
+    public class Solution
+    {
+        static void checkMagazine(string[] magazine, string[] note)
+        {
+            Console.Out.WriteLine(getCheckMagazineResult(magazine, note));
+        }
+
+        public static string getCheckMagazineResult(string[] magazine, string[] note)
+        {
+            var magazineWords = new Dictionary<string, int>();
+
+            foreach (var word in magazine)
+            {
+                if(!magazineWords.ContainsKey(word))
+                {
+                    magazineWords.Add(word, 0);
+                }
+
+                magazineWords[word] = magazineWords[word] + 1;
+            }
+
+            foreach (var word in note)
+            {
+                if (!magazineWords.ContainsKey(word))
+                {
+                    return "No";
+                }
+
+                if (magazineWords[word] < 1)
+                {
+                    return "No";
+                }
+
+                magazineWords[word] = magazineWords[word] - 1;
+            }
+
+            return "Yes";
+        }
+    }
+}
