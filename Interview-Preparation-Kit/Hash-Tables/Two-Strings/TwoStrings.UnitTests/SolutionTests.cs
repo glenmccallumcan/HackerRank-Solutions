07fdcad6e183f5e0a68709c0a3ed6a15fb3ce2fe
Glen McCallum <glen.mccallum@protonmail.com>
2019-04-13 08:47:29 -0700

    Added solution for Two Strings Problem
    - easy, easy. nothing to see here

---
+++ Interview-Preparation-Kit/Hash-Tables/Two-Strings/TwoStrings.UnitTests/SolutionTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace TwoStrings.UnitTests
+{
+    public class SolutionTests
+    {
+        public class TheTwoStringsMethod
+        {
+            [Theory]
+            [InlineData("hello", "world", "YES")]
+            [InlineData("hi", "world", "NO")]
+            public void ComputesCorrectResponse(string s1, string s2, string response)
+            {
+                Assert.Equal(Solution.twoStrings(s1, s2), response);
+            }
+        }
+    }
+}
---
+++ Interview-Preparation-Kit/Hash-Tables/Two-Strings/TwoStrings/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace TwoStrings
+{
+    public class Solution
+    {
+        public static string twoStrings(string s1, string s2)
+        {
+            if(s1.ToCharArray().Intersect(s2.ToCharArray()).Any())
+            {
+                return "YES";
+            }
+
+            return "NO";
+        }
+    }
+}
