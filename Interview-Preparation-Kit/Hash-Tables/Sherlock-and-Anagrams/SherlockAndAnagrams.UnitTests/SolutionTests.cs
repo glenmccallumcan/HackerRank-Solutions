cb69e0bd9caa49d22976f83963565477826767c1
Glen McCallum <glen.mccallum@protonmail.com>
2019-04-16 09:28:02 -0700

    Added initial attempt for Sherlock and Anagrams
    - brute force approach. times out due to nested
    looping

---
+++ Interview-Preparation-Kit/Hash-Tables/Sherlock-and-Anagrams/SherlockAndAnagrams.UnitTests/SolutionTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Xunit;
+
+namespace SherlockAndAnagrams.UnitTests
+{
+    public class SolutionTests
+    {
+        public class TheSherlockAndAnagramsMethod
+        {
+            [Theory]
+            [InlineData("abba", 4)]
+            [InlineData("abcd", 0)]
+            public void ComputesCorrectResponse(string s, int result)
+            {
+                Assert.Equal(Solution.sherlockAndAnagrams(s), result);
+            }
+        }
+
+        public class TheAreAnagramsMethod
+        {
+            [Theory]
+            [InlineData("abc", "bcd", false)]
+            [InlineData("a", "a", true)]
+            [InlineData("a", "b", false)]
+            [InlineData("ab", "ba", true)]
+            public void ComputesCorrectResponse(string left, string right, bool result)
+            {
+                Assert.Equal(Solution.areAnagrams(left, right), result);
+            }
+        }
+
+        public class countAnagramsOfLength
+        {
+            [Theory]
+            [InlineData("abc", "a", 0, 1)]
+            [InlineData("abc", "a", 1, 0)]
+            [InlineData("a", "", 0, 0)]
+            [InlineData("abba", "ab", 1, 1)]
+            [InlineData("abba", "ab", 2, 1)]
+            [InlineData("abba", "ab", 3, 0)]
+            public void ComputesCorrectResponse(string s, string sub, int startIndex, int result)
+            {
+                Assert.Equal(Solution.countAnagramsToRightOf(s, sub, startIndex), result);
+            }
+        }
+
+        public class countAllAnagramsOfLength
+        {
+            [Theory]
+            [InlineData("abc", 0, 0)]
+            [InlineData("abc", 1, 0)]
+            [InlineData("abc", 2, 0)]
+            [InlineData("abc", 3, 0)]
+            [InlineData("abc", 4, 0)]
+            [InlineData("a", 1, 0)]
+            [InlineData("abba", 1, 2)]
+            [InlineData("abba", 2, 1)]
+            [InlineData("abba", 3, 1)]
+            public void ComputesCorrectResponse(string s, int anagramLength, int result)
+            {
+                Assert.Equal(Solution.countAllAnagramsOfLength(s, anagramLength), result);
+            }
+        }
+    }
+}
---
+++ Interview-Preparation-Kit/Hash-Tables/Sherlock-and-Anagrams/SherlockAndAnagrams/Solution.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+
+namespace SherlockAndAnagrams
+{
+    public class Solution
+    {
+        public static int sherlockAndAnagrams(string s)
+        {
+            if(s.ToCharArray().Distinct().Count() == s.ToCharArray().Length)
+            {
+                return 0;
+            }
+
+            var counter = 0;
+            
+            for(int i = 1; i < s.Length; i++)
+            {
+                counter += countAllAnagramsOfLength(s, i);
+            }
+
+            return counter;
+        }
+
+        public static int countAllAnagramsOfLength(string s, int anagramLength)
+        {
+            var index = 0;
+            int counter = 0;
+
+            while(anagramLength != 0 && (index + anagramLength -1) < s.Length)
+            {
+                var sub = s.Substring(index, anagramLength);
+                counter += countAnagramsToRightOf(s, sub, index + 1);
+                index++;
+            }
+            return counter;
+        }
+
+        public static int countAnagramsToRightOf(string s, string sub, int startIndex)
+        {
+            int left = startIndex;
+            int counter = 0;
+
+            while (sub.Length != 0 && left + sub.Length - 1 < s.Length)
+            {
+                if(areAnagrams(sub, s.Substring(left, sub.Length)))
+                {
+                    counter++;
+                }
+
+                left++;
+            }
+
+            return counter;
+        }
+
+        public static bool areAnagrams(string left, string right)
+        {
+            if(left.ToCharArray().OrderBy(c => c).SequenceEqual(right.ToCharArray().OrderBy(c => c)))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
