a3413fd9add19cdae4a3b8707c61951d06bfb176
Glen McCallum <glen.mccallum@protonmail.com>
2019-05-28 08:42:30 -0700

    Added solution for sorting > bubble sort

---
+++ Interview-Preparation-Kit/Sorting/Bubble-Sort/Solution.UnitTests/SolutionTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Solution.UnitTests
+{
+    public class SolutionTests
+    {
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3 }, new int[] { 0, 1, 3 })]
+        [InlineData(new int[] { 3, 2, 1 }, new int[] { 3, 1, 3 })]
+        [InlineData(new int[] { 4, 2, 3, 1 }, new int[] { 5, 1, 4 })]
+        public void ComputesCorrectResponse(int[] a, int[] result)
+        {
+            Assert.Equal(Solution.myCountSwaps(a), result);
+        }
+    }
+}
---
+++ Interview-Preparation-Kit/Sorting/Bubble-Sort/Solution/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solution
+{
+    public class Solution
+    {
+        public static void countSwaps(int[] a)
+        {
+            var answer = myCountSwaps(a);
+
+            if (answer.Length != 3)
+            {
+                throw new NotSupportedException();
+            }
+
+            Console.WriteLine($"Array is sorted in {answer[0]} swaps.");
+            Console.WriteLine($"First Element: {answer[1]}");
+            Console.WriteLine($"Last Element: {answer[2]}");
+        }
+
+        public static int[] myCountSwaps(int[] a)
+        {
+            var n = a.Length;
+            var counter = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int j = 0; j < n - 1; j++)
+                {
+                    // Swap adjacent elements if they are in decreasing order
+                    if (a[j] > a[j + 1])
+                    {
+                        swap(a, j, j + 1);
+                        counter++;
+                    }
+                }
+            }
+
+            return new int[] { counter, a[0], a[n - 1] };
+        }
+
+        private static void swap(int[] array, int index1, int index2)
+        {
+            var temp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp;
+        }
+    }
+}
