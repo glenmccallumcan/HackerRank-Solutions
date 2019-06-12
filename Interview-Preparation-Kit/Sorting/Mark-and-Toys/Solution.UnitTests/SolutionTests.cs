ae4799b7f9114ee8d7586a32fa5c471465d5ee84
Glen McCallum <glen.mccallum@protonmail.com>
2019-06-12 08:11:03 -0700

    Added solution for Hackerrank Mark and Toys

---
+++ Interview-Preparation-Kit/Sorting/Mark-and-Toys/Solution.UnitTests/SolutionTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Solution.UnitTests
+{
+    public class SolutionTests
+    {
+
+
+        [Theory]
+        [InlineData(new int[] {1, 12, 5, 111, 200, 1000, 10}, 50, 4)]
+        public void ComputesCorrectResponse(int[] toys, int money, int numberOfToys)
+        {
+            Assert.Equal(Solution.maximumToys(toys, money), numberOfToys);
+        }
+    }
+}
---
+++ Interview-Preparation-Kit/Sorting/Mark-and-Toys/Solution/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solution
+{
+    public static class Solution
+    {
+        // Complete the maximumToys function below.
+        public static int maximumToys(int[] prices, int k)
+        {
+            var orderedPrices = prices.OrderBy(p => p);
+
+            var count = 0;
+            var spent = 0;
+            foreach(var price in orderedPrices)
+            {
+                if (spent + price > k)
+                {
+                    break;
+                }
+                else
+                {
+                    count++;
+                    spent += price;
+                }
+            }
+
+            return count;
+        }
+    }
+}
