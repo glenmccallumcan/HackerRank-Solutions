2e94f9443188398bd701eec28a3b399aa4aa6124
Glen McCallum <glen.mccallum@protonmail.com>
2019-06-20 07:39:03 -0700

    Added a solution for Counting-Inversions
    - basically a straight merge sort algorithm from one
    of my old comp sci textbooks. datatypes and a few other
    changes
    - then added a bit of extra code to count the inversions

---
+++ Interview-Preparation-Kit/Sorting/Counting-Inversions/Solution.UnitTests/SolutionTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Solution.UnitTests
+{
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 1, 1, 2, 2 }, 0)]
+        [InlineData(new int[] { 2, 1, 3, 1, 2 }, 4)]
+        [InlineData(new int[] { 1, 5, 3, 7 }, 1)]
+        [InlineData(new int[] { 3, 2, 1 }, 3)]
+        [InlineData(new int[] { 7, 5, 3, 1 }, 6)]
+        public void ComputesCorrectResponse(int[] arr, int result)
+        {
+            Assert.Equal(Solution.countInversions(arr), result);
+        }
+    }
+}
---
+++ Interview-Preparation-Kit/Sorting/Counting-Inversions/Solution/Solution.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solution
+{
+    public static class Solution
+    {
+        public static long countInversions(int[] arr)
+        {
+            int[] temp = new int[arr.Length];
+
+            return MergeSort(arr, temp, 0, arr.Length - 1);
+        }
+
+        private static long MergeSort(int[] numbers, int[] temp, int left, int right)
+        {
+            long leftInversions = 0;
+            long rightInversions = 0;
+            long mergeInversions = 0;
+            if( left < right )
+            {
+                int centre = (left + right) / 2;
+                leftInversions = MergeSort(numbers, temp, left, centre);
+                rightInversions = MergeSort(numbers, temp, centre + 1, right);
+                mergeInversions = Merge(numbers, temp, left, centre + 1, right);
+            }
+
+            return leftInversions + rightInversions + mergeInversions;
+        }
+
+        private static long Merge(int[] numbers, int[] temp, int leftPos, int rightPos, int rightEnd)
+        {
+            int leftEnd = rightPos - 1;
+            int tempPos = leftPos;
+            int numElements = rightEnd - leftPos + 1;
+            long inversionCount = 0;
+
+            while (leftPos <= leftEnd && rightPos <= rightEnd)
+            {
+                if (numbers[leftPos] <= numbers[rightPos])
+                {
+                    temp[tempPos++] = numbers[leftPos++];
+                }
+                else
+                {
+                    inversionCount += leftEnd - leftPos + 1;
+                    temp[tempPos++] = numbers[rightPos++];
+                }
+            }
+
+            while(leftPos <= leftEnd)
+            {
+                temp[tempPos++] = numbers[leftPos++];
+            }
+            
+            while(rightPos <= rightEnd)
+            {
+                temp[tempPos++] = numbers[rightPos++];
+            }
+
+            for(int i = 0; i < numElements; i++, rightEnd--)
+            {
+                numbers[rightEnd] = temp[rightEnd];
+            }
+
+            return inversionCount;
+        }
+    }
+}
